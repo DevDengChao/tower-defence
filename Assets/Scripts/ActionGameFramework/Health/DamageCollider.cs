f8376ee19041f1b25d98c46d74fe33c030e40eb0
XieEDeHeiShou <2325690622@qq.com>
2019-04-11 22:29:09 +0800

    Reformat DamageCollider

--- Assets/Scripts/ActionGameFramework/Health/DamageCollider.cs
+++ Assets/Scripts/ActionGameFramework/Health/DamageCollider.cs
@@ -1,55 +1,52 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ActionGameFramework.Health
 {
-	/// <summary>
-	/// Damage collider - a collider based implementation of DamageZone
-	/// </summary>
-	[RequireComponent(typeof(Collider))]
-	public class DamageCollider : DamageZone
-	{
-		/// <summary>
-		/// On collision enter, see if the colliding object has a Damager and then make the damageableBehaviour take damage
-		/// </summary>
-		/// <param name="c">The collider</param>
-		protected void OnCollisionEnter(Collision c)
-		{
-			var damager = c.gameObject.GetComponent<Damager>();
-			if (damager == null)
-			{
-				return;
-			}
-			LazyLoad();
-			
-			float scaledDamage = ScaleDamage(damager.damage);
-			Vector3 collisionPosition = ConvertContactsToPosition(c.contacts);
-			damageableBehaviour.TakeDamage(scaledDamage, collisionPosition, damager.alignmentProvider);
-			
-			damager.HasDamaged(collisionPosition, damageableBehaviour.configuration.alignmentProvider);
-		}
+    /// <inheritdoc />
+    /// <summary>
+    /// Damage collider - a collider based implementation of DamageZone
+    /// </summary>
+    [RequireComponent(typeof(Collider))]
+    public class DamageCollider : DamageZone
+    {
+        /// <summary>
+        /// On collision enter, see if the colliding object has a Damager and then make the damageableBehaviour take damage
+        /// </summary>
+        /// <param name="c">The collider</param>
+        protected void OnCollisionEnter(Collision c)
+        {
+            var damager = c.gameObject.GetComponent<Damager>();
+            if (damager == null)
+            {
+                return;
+            }
 
-		/// <summary>
-		/// Averages the contacts to get the position.
-		/// </summary>
-		/// <returns>The average position.</returns>
-		/// <param name="contacts">Contacts.</param>
-		protected Vector3 ConvertContactsToPosition(ContactPoint[] contacts)
-		{
-			Vector3 output = Vector3.zero;
-			int length = contacts.Length;
+            LazyLoad();
 
-			if (length == 0)
-			{
-				return output;
-			}
+            var scaledDamage = ScaleDamage(damager.damage);
+            var collisionPosition = ConvertContactsToPosition(c.contacts);
+            damageableBehaviour.TakeDamage(scaledDamage, collisionPosition, damager.alignmentProvider);
 
-			for (int i = 0; i < length; i++)
-			{
-				output += contacts[i].point;
-			}
+            damager.HasDamaged(collisionPosition, damageableBehaviour.configuration.alignmentProvider);
+        }
 
-			output = output / length;
-			return output;
-		}
-	}
+        /// <summary>
+        /// Averages the contacts to get the position.
+        /// </summary>
+        /// <returns>The average position.</returns>
+        /// <param name="contacts">Contacts.</param>
+        private static Vector3 ConvertContactsToPosition(IList<ContactPoint> contacts)
+        {
+            var output = Vector3.zero;
+            var length = contacts.Count;
+
+            if (length == 0) return output;
+
+            for (var i = 0; i < length; i++) output += contacts[i].point;
+
+            output = output / length;
+            return output;
+        }
+    }
 }
