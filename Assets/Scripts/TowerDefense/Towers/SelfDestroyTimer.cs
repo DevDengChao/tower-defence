859c78c6da2184e4302e4a1ebebe82b543b75d8a
XieEDeHeiShou <2325690622@qq.com>
2019-04-11 22:43:16 +0800

    Reformat SelfDestroyTimer

--- Assets/Scripts/TowerDefense/Towers/SelfDestroyTimer.cs
+++ Assets/Scripts/TowerDefense/Towers/SelfDestroyTimer.cs
@@ -4,61 +4,60 @@ using UnityEngine.Events;
 
 namespace TowerDefense.Towers
 {
-	/// <summary>
-	/// A helper component for self destruction
-	/// </summary>
-	public class SelfDestroyTimer : MonoBehaviour
-	{
-		/// <summary>
-		/// The time before destruction
-		/// </summary>
-		public float time = 5;
+    /// <inheritdoc />
+    /// <summary>
+    /// A helper component for self destruction
+    /// </summary>
+    public sealed class SelfDestroyTimer : MonoBehaviour
+    {
+        /// <summary>
+        /// The time before destruction
+        /// </summary>
+        public float time = 5;
 
-		/// <summary>
-		/// The controlling timer
-		/// </summary>
-		public Timer timer;
-		
-		/// <summary>
-		/// The exposed death callback
-		/// </summary>
-		public UnityEvent death;
+        /// <summary>
+        /// The controlling timer
+        /// </summary>
+        private Timer _timer;
 
-		/// <summary>
-		/// Potentially initialize the time if necessary
-		/// </summary>
-		protected virtual void OnEnable()
-		{
-			if (timer == null)
-			{
-				timer = new Timer(time, OnTimeEnd);
-			}
-			else
-			{
-				timer.Reset();
-			}
-		}
+        /// <summary>
+        /// The exposed death callback
+        /// </summary>
+        public UnityEvent death;
 
-		/// <summary>
-		/// Update the timer
-		/// </summary>
-		protected virtual void Update()
-		{
-			if (timer == null)
-			{
-				return;
-			}
-			timer.Tick(Time.deltaTime);
-		}
+        /// <summary>
+        /// Potentially initialize the time if necessary
+        /// </summary>
+        private void OnEnable()
+        {
+            if (_timer == null)
+            {
+                _timer = new Timer(time, OnTimeEnd);
+            }
+            else
+            {
+                _timer.Reset();
+            }
+        }
 
-		/// <summary>
-		/// Fires at the end of timer
-		/// </summary>
-		protected virtual void OnTimeEnd()
-		{
-			death.Invoke();
-			Poolable.TryPool(gameObject);
-			timer.Reset();
-		}
-	}
+        /// <summary>
+        /// Update the timer
+        /// </summary>
+        private void Update()
+        {
+            if (_timer == null) return;
+
+            _timer.Tick(Time.deltaTime);
+        }
+
+        /// <summary>
+        /// Fires at the end of timer
+        /// </summary>
+        private void OnTimeEnd()
+        {
+            death.Invoke();
+            Poolable.TryPool(gameObject);
+            _timer.Reset();
+        }
+    }
 }
