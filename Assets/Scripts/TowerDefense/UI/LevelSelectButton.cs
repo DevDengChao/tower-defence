09fb8d2b8fc517110bab0bb7f70e37c67843fee3
XieEDeHeiShou <2325690622@qq.com>
2019-04-15 23:09:56 +0800

    Reformat LevelSelectButton

--- Assets/Scripts/TowerDefense/UI/LevelSelectButton.cs
+++ Assets/Scripts/TowerDefense/UI/LevelSelectButton.cs
@@ -7,117 +7,120 @@ using UnityEngine.UI;
 
 namespace TowerDefense.UI
 {
-	/// <summary>
-	/// The button for selecting a level
-	/// </summary>
-	[RequireComponent(typeof(Button))]
-	public class LevelSelectButton : MonoBehaviour, ISelectHandler
-	{
-		/// <summary>
-		/// Reference to the required button component
-		/// </summary>
-		protected Button m_Button;
+    /// <inheritdoc cref="MonoBehaviour" />
+    /// <inheritdoc cref="ISelectHandler"/>
+    /// <summary>
+    /// The button for selecting a level
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public class LevelSelectButton : MonoBehaviour, ISelectHandler
+    {
+        /// <summary>
+        /// Reference to the required button component
+        /// </summary>
+        private Button _mButton;
 
-		/// <summary>
-		/// The UI text element that displays the name of the level
-		/// </summary>
-		public Text titleDisplay;
-		
-		public Text description;
+        /// <summary>
+        /// The UI text element that displays the name of the level
+        /// </summary>
+        public Text titleDisplay;
 
-		public Sprite starAchieved;
+        public Text description;
 
-		public Image[] stars;
+        public Sprite starAchieved;
 
-		protected MouseScroll m_MouseScroll;
+        public Image[] stars;
 
-		/// <summary>
-		/// The data concerning the level this button displays
-		/// </summary>
-		protected LevelItem m_Item;
+        private MouseScroll _mMouseScroll;
 
-		/// <summary>
-		/// When the user clicks the button, change the scene
-		/// </summary>
-		public void ButtonClicked()
-		{
-			ChangeScenes();
-		}
+        /// <summary>
+        /// The data concerning the level this button displays
+        /// </summary>
+        private LevelItem _mItem;
 
-		/// <summary>
-		/// A method for assigning the data from item to the button
-		/// </summary>
-		/// <param name="item">
-		/// The data with the information concerning the level
-		/// </param>
-		public void Initialize(LevelItem item, MouseScroll mouseScroll)
-		{
-			LazyLoad();
-			if (titleDisplay == null)
-			{
-				return;
-			}
-			m_Item = item;
-			titleDisplay.text = item.name;
-			description.text = item.description;
-			HasPlayedState();
-			m_MouseScroll = mouseScroll;
-		}
+        /// <summary>
+        /// When the user clicks the button, change the scene
+        /// </summary>
+        public void ButtonClicked()
+        {
+            ChangeScenes();
+        }
 
-		/// <summary>
-		/// Configures the feedback concerning if the player has played
-		/// </summary>
-		protected void HasPlayedState()
-		{
-			GameManager gameManager = GameManager.instance;
-			if (gameManager == null)
-			{
-				return;
-			}
-			int starsForLevel = gameManager.GetStarsForLevel(m_Item.id);
-			for (int i = 0; i < starsForLevel; i++)
-			{
-				stars[i].sprite = starAchieved;
-			}
-		}
+        /// <summary>
+        /// A method for assigning the data from item to the button
+        /// </summary>
+        /// <param name="item">
+        /// The data with the information concerning the level
+        /// </param>
+        /// <param name="mouseScroll">
+        /// The MouseScroll component
+        /// </param>
+        public void Initialize(LevelItem item, MouseScroll mouseScroll)
+        {
+            LazyLoad();
+            if (titleDisplay == null)
+            {
+                return;
+            }
 
-		/// <summary>
-		/// Changes the scene to the scene name provided by m_Item
-		/// </summary>
-		protected void ChangeScenes()
-		{
-			SceneManager.LoadScene(m_Item.sceneName);
-		}
+            _mItem = item;
+            titleDisplay.text = item.name;
+            description.text = item.description;
+            HasPlayedState();
+            _mMouseScroll = mouseScroll;
+        }
 
-		/// <summary>
-		/// Ensure <see cref="m_Button"/> is not null
-		/// </summary>
-		protected void LazyLoad()
-		{
-			if (m_Button == null)
-			{
-				m_Button = GetComponent<Button>();
-			}
-		}
+        /// <summary>
+        /// Configures the feedback concerning if the player has played
+        /// </summary>
+        private void HasPlayedState()
+        {
+            var gameManager = GameManager.instance;
+            if (gameManager == null) return;
 
-		/// <summary>
-		/// Remove all listeners on the button before destruction
-		/// </summary>
-		protected void OnDestroy()
-		{
-			if (m_Button != null)
-			{
-				m_Button.onClick.RemoveAllListeners();
-			}
-		}
+            var starsForLevel = gameManager.GetStarsForLevel(_mItem.id);
+            // override image sprite
+            for (var i = 0; i < starsForLevel; i++) stars[i].sprite = starAchieved;
+        }
 
-		/// <summary>
-		/// Implementation of ISelectHandler
-		/// </summary>
-		/// <param name="eventData">Select event data</param>
-		public void OnSelect(BaseEventData eventData)
-		{
-			m_MouseScroll.SelectChild(this);
-		}
-	}
+        /// <summary>
+        /// Changes the scene to the scene name provided by m_Item
+        /// </summary>
+        private void ChangeScenes()
+        {
+            SceneManager.LoadScene(_mItem.sceneName);
+        }
+
+        /// <summary>
+        /// Ensure <see cref="_mButton"/> is not null
+        /// </summary>
+        private void LazyLoad()
+        {
+            if (_mButton == null)
+            {
+                _mButton = GetComponent<Button>();
+            }
+        }
+
+        /// <summary>
+        /// Remove all listeners on the button before destruction
+        /// </summary>
+        protected void OnDestroy()
+        {
+            if (_mButton != null)
+            {
+                _mButton.onClick.RemoveAllListeners();
+            }
+        }
+
+        /// <inheritdoc cref="ISelectHandler.OnSelect"/>
+        /// <summary>
+        /// Implementation of ISelectHandler
+        /// </summary>
+        /// <param name="eventData">Select event data</param>
+        public void OnSelect(BaseEventData eventData)
+        {
+            _mMouseScroll.SelectChild(this);
+        }
+    }
 }
