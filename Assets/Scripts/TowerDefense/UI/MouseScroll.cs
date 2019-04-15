80c2fcc307afc86b48611133a0d5ed056636d97b
XieEDeHeiShou <2325690622@qq.com>
2019-04-15 23:09:29 +0800

    Reformat MouseScroll

--- Assets/Scripts/TowerDefense/UI/MouseScroll.cs
+++ Assets/Scripts/TowerDefense/UI/MouseScroll.cs
@@ -4,101 +4,99 @@ using UnityInput = UnityEngine.Input;
 
 namespace TowerDefense.UI
 {
-	/// <summary>
-	/// Component to override ScrollRect, uses normalized mouse position inside ScrollRect to scroll
-	/// </summary>
-	[RequireComponent(typeof(ScrollRect))]
-	public class MouseScroll : MonoBehaviour
-	{
-		/// <summary>
-		/// If the normalized scroll position should be clamped between 0 & 1
-		/// </summary>
-		public bool clampScroll = true;
+    /// <inheritdoc />
+    /// <summary>
+    /// Component to override ScrollRect, uses normalized mouse position inside ScrollRect to scroll
+    /// </summary>
+    [RequireComponent(typeof(ScrollRect))]
+    public class MouseScroll : MonoBehaviour
+    {
+        /// <summary>
+        /// If the normalized scroll position should be clamped between 0 & 1
+        /// </summary>
+        public bool clampScroll = true;
 
-		/// <summary>
-		/// Buffer to adjust ScrollRect size
-		/// </summary>
-		public float scrollXBuffer;
-		
-		public float scrollYBuffer;
+        /// <summary>
+        /// Buffer to adjust ScrollRect size
+        /// </summary>
+        public float scrollXBuffer;
 
-		protected ScrollRect m_ScrollRect;
-		protected RectTransform m_ScrollRectTransform;
+        public float scrollYBuffer;
 
-		protected bool m_OverrideScrolling,
-		               m_HasRightBuffer;
+        private ScrollRect _mScrollRect;
+        private RectTransform _mScrollRectTransform;
 
-		public void SetHasRightBuffer(bool rightBuffer)
-		{
-			m_HasRightBuffer = rightBuffer;
-		}
+        private bool _mOverrideScrolling,
+            _mHasRightBuffer;
 
-		/// <summary>
-		/// If appropriate, we cache ScrollRect reference, disable it and enable scrolling override
-		/// </summary>
-		void Start()
-		{
+        public void SetHasRightBuffer(bool rightBuffer)
+        {
+            _mHasRightBuffer = rightBuffer;
+        }
+
+        /// <summary>
+        /// If appropriate, we cache ScrollRect reference, disable it and enable scrolling override
+        /// </summary>
+        private void Start()
+        {
 #if UNITY_STANDALONE || UNITY_EDITOR
-			m_ScrollRect = GetComponent<ScrollRect>();
-			m_ScrollRect.enabled = false;
-			m_OverrideScrolling = true;
-			m_ScrollRectTransform = (RectTransform) m_ScrollRect.transform;
+            _mScrollRect = GetComponent<ScrollRect>();
+            _mScrollRect.enabled = false;
+            _mOverrideScrolling = true;
+            _mScrollRectTransform = (RectTransform) _mScrollRect.transform;
 #else
-			m_OverrideScrolling = false;
+			_mOverrideScrolling = false;
 #endif
-		}
+        }
 
-		/// <summary>
-		///  Use normalized mouse position inside ScrollRect to scroll
-		/// </summary>
-		void Update()
-		{
-			if (!m_OverrideScrolling)
-			{
-				return;
-			}
-			Vector3 mousePosition = UnityInput.mousePosition;
+        /// <summary>
+        ///  Use normalized mouse position inside ScrollRect to scroll
+        /// </summary>
+        private void Update()
+        {
+            if (!_mOverrideScrolling)
+            {
+                return;
+            }
 
-			// only scroll if mouse is inside ScrollRect
-			bool inside = RectTransformUtility.RectangleContainsScreenPoint(m_ScrollRectTransform, mousePosition);
-			if (!inside)
-			{
-				return;
-			}
+            var mousePosition = UnityInput.mousePosition;
 
-			Rect rect = m_ScrollRectTransform.rect;
-			float adjustmentX = rect.width * scrollXBuffer,
-			      adjustmentY = rect.height * scrollYBuffer;
+            // only scroll if mouse is inside ScrollRect
+            var inside = RectTransformUtility.RectangleContainsScreenPoint(_mScrollRectTransform, mousePosition);
+            if (!inside) return;
 
-			Vector2 localPoint;
-			RectTransformUtility.ScreenPointToLocalPointInRectangle(m_ScrollRectTransform, mousePosition, null, out localPoint);
+            var rect = _mScrollRectTransform.rect;
+            var adjustmentX = rect.width * scrollXBuffer;
+            var adjustmentY = rect.height * scrollYBuffer;
 
-			Vector2 pivot = m_ScrollRectTransform.pivot;
-			float x = (localPoint.x + (rect.width - adjustmentX) * pivot.x) / (rect.width - 2 * adjustmentX);
-			float y = (localPoint.y + (rect.height - adjustmentY) * pivot.y) / (rect.height - 2 * adjustmentY);
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_mScrollRectTransform, mousePosition, null,
+                out localPoint);
 
-			if (clampScroll)
-			{
-				x = Mathf.Clamp01(x);
-				y = Mathf.Clamp01(y);
-			}
+            var pivot = _mScrollRectTransform.pivot;
+            var x = (localPoint.x + (rect.width - adjustmentX) * pivot.x) / (rect.width - 2 * adjustmentX);
+            var y = (localPoint.y + (rect.height - adjustmentY) * pivot.y) / (rect.height - 2 * adjustmentY);
 
-			m_ScrollRect.normalizedPosition = new Vector2(x, y);
-		}
+            if (clampScroll)
+            {
+                x = Mathf.Clamp01(x);
+                y = Mathf.Clamp01(y);
+            }
 
-		/// <summary>
-		/// Called when a button inside the scroll is selected
-		/// </summary>
-		/// <param name="levelSelectButton">Selected child</param>
-		public void SelectChild(LevelSelectButton levelSelectButton)
-		{
-			// minus one if  buffer
-			int childCount = levelSelectButton.transform.parent.childCount - (m_HasRightBuffer ? 1 : 0);
-			if (childCount > 1)
-			{
-				float normalized = (float)levelSelectButton.transform.GetSiblingIndex() / ( childCount - 1);
-				m_ScrollRect.normalizedPosition = new Vector2(normalized, 0);
-			}
-		}
-	}
+            _mScrollRect.normalizedPosition = new Vector2(x, y);
+        }
+
+        /// <summary>
+        /// Called when a button inside the scroll is selected
+        /// </summary>
+        /// <param name="levelSelectButton">Selected child</param>
+        public void SelectChild(LevelSelectButton levelSelectButton)
+        {
+            // minus one if  buffer
+            var childCount = levelSelectButton.transform.parent.childCount - (_mHasRightBuffer ? 1 : 0);
+            if (childCount <= 1) return;
+            var normalized = (float) levelSelectButton.transform.GetSiblingIndex() / (childCount - 1);
+            _mScrollRect.normalizedPosition = new Vector2(normalized, 0);
+        }
+    }
 }
